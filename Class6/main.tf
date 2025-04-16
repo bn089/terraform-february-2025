f3fed1b5e92b8bf69a23dce5e16ed55ef43f8eeb
bn089 <nurkamilbubusara@gmail.com>
2025-04-16 02:10:34 +0000

    edited class6

--- Class6/main.tf
+++ Class6/main.tf
@@ -1,11 +1,9 @@
-module "vpc1" {
-  source  = "bn089/vpc1/aws"
+module "vpc-1" {
+  source  = "bn089/vpc-1/aws"
   version = "0.2.0"
 
-
-
- vpc_cidr = "172.31.0.0/16"
- subnet1_cidr = "172.31.1.0/24"
- subnet2_cidr = "172.31.2.0/24"
- environment = "qa"
+  vpc_cidr = "172.31.0.0/16"
+  subnet1_cidr = "172.31.1.0/24"
+  subnet2_cidr = "172.31.2.0/24"
+  environment = "qa"
 }
